2fe8551b72ee15dcd3759a5a5ef246c9cb607c5f
Chained181 <85314212+Chained181@users.noreply.github.com>
2021-06-05 11:54:18 +0300

    Second Commit
    Second Commit

--- Assets/Scripts/BlockMove.cs
+++ Assets/Scripts/BlockMove.cs
@@ -37,120 +37,126 @@ public class BlockMove : MonoBehaviour/*, IBeginDragHandler, IDragHandler*/
 
     void KeyboardControls()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Time.deltaTime > 0)
         {
-            transform.position += new Vector3(-1, 0, 0);
-            if (!ValidMove())
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
-                transform.position -= new Vector3(-1, 0, 0);
+                transform.position += new Vector3(-1, 0, 0);
+                if (!ValidMove())
+                {
+                    transform.position -= new Vector3(-1, 0, 0);
+                }
             }
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            transform.position += new Vector3(1, 0, 0);
-            if (!ValidMove())
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
             {
-                transform.position -= new Vector3(1, 0, 0);
+                transform.position += new Vector3(1, 0, 0);
+                if (!ValidMove())
+                {
+                    transform.position -= new Vector3(1, 0, 0);
+                }
             }
-        }
-        else if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
-            if (!ValidMove())
+            else if (Input.GetKeyDown(KeyCode.UpArrow))
             {
-                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
+                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
+                if (!ValidMove())
+                {
+                    transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
+                }
             }
-        }
 
-        if (Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime))
-        {
-            transform.position += new Vector3(0, -1, 0);
-            previousTime = Time.time;
-            if (!ValidMove())
+            if (Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime))
             {
-                transform.position -= new Vector3(0, -1, 0);
-                AddToGrid();
-                CheckForLines();
-                this.enabled = false;
-                FindObjectOfType<Spawner>().NewQube();
+                transform.position += new Vector3(0, -1, 0);
+                previousTime = Time.time;
+                if (!ValidMove())
+                {
+                    transform.position -= new Vector3(0, -1, 0);
+                    AddToGrid();
+                    CheckForLines();
+                    this.enabled = false;
+                    FindObjectOfType<Spawner>().NewQube();
+                }
             }
-        }
 
+        }
     }
 
     void TouchControls()
-    { 
-        if(Input.touchCount > 0)
-        {
-            Touch t = Input.GetTouch(0);
-
-            if (t.phase == TouchPhase.Began)
+    {
+        if (Time.deltaTime > 0)
+        { 
+            if (Input.touchCount > 0)
             {
-                previousUnitPosition = new Vector2(t.position.x, t.position.y);
+                Touch t = Input.GetTouch(0);
 
-            }
-            else if (t.phase == TouchPhase.Moved)
-            {
-                Vector2 touchDeltaPosition = t.deltaPosition;
-                direction = touchDeltaPosition.normalized;
-                if(Mathf.Abs(t.position.x - previousUnitPosition.x)>= touchSensitivityHorizontal && direction.x < 0 && t.deltaPosition.y > -10 && t.deltaPosition.y <10)
+                if (t.phase == TouchPhase.Began)
                 {
-                    //Left
-                    transform.position += new Vector3(-1, 0, 0);
-                    if (!ValidMove())
-                    {
-                        transform.position -= new Vector3(-1, 0, 0);
-                    }
-                    previousUnitPosition = t.position;
-                    moved = true;
+                    previousUnitPosition = new Vector2(t.position.x, t.position.y);
 
-                } else if (Mathf.Abs(t.position.x - previousUnitPosition.x) >= touchSensitivityHorizontal && direction.x > 0 && t.deltaPosition.y > -10 && t.deltaPosition.y < 10)
-                {
-                    //Right
-                    transform.position += new Vector3(1, 0, 0);
-                    if (!ValidMove())
-                    {
-                        transform.position -= new Vector3(1, 0, 0);
-                    }
-                    previousUnitPosition = t.position;
-                    moved = true;
                 }
-                else if(Mathf.Abs(t.position.y - previousUnitPosition.y) >= touchSensitivityVertical && direction.y < 0 && t.deltaPosition.x > -10 && t.deltaPosition.x < 10)
+                else if (t.phase == TouchPhase.Moved)
                 {
-                    //Down
-                    transform.position += new Vector3(0, -1, 0);
-                    previousTime = Time.time;
-                    if (!ValidMove())
+                    Vector2 touchDeltaPosition = t.deltaPosition;
+                    direction = touchDeltaPosition.normalized;
+                    if (Mathf.Abs(t.position.x - previousUnitPosition.x) >= touchSensitivityHorizontal && direction.x < 0 && t.deltaPosition.y > -10 && t.deltaPosition.y < 10)
                     {
-                        transform.position -= new Vector3(0, -1, 0);
-                        AddToGrid();
-                        CheckForLines();
-                        this.enabled = false;
-                        FindObjectOfType<Spawner>().NewQube();
+                        //Left
+                        transform.position += new Vector3(-1, 0, 0);
+                        if (!ValidMove())
+                        {
+                            transform.position -= new Vector3(-1, 0, 0);
+                        }
+                        previousUnitPosition = t.position;
+                        moved = true;
+
                     }
-                    previousUnitPosition = t.position;
-                    moved = true;
-                }
-            }
-
-
-        
-            else if (t.phase == TouchPhase.Ended)
-            {
-                if (!moved && t.position.x > Screen.width / 4)
-                {
-                    transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
-                    if (!ValidMove())
+                    else if (Mathf.Abs(t.position.x - previousUnitPosition.x) >= touchSensitivityHorizontal && direction.x > 0 && t.deltaPosition.y > -10 && t.deltaPosition.y < 10)
                     {
-                        transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
+                        //Right
+                        transform.position += new Vector3(1, 0, 0);
+                        if (!ValidMove())
+                        {
+                            transform.position -= new Vector3(1, 0, 0);
+                        }
+                        previousUnitPosition = t.position;
+                        moved = true;
+                    }
+                    else if (Mathf.Abs(t.position.y - previousUnitPosition.y) >= touchSensitivityVertical && direction.y < 0 && t.deltaPosition.x > -10 && t.deltaPosition.x < 10)
+                    {
+                        //Down
+                        transform.position += new Vector3(0, -1, 0);
+                        previousTime = Time.time;
+                        if (!ValidMove())
+                        {
+                            transform.position -= new Vector3(0, -1, 0);
+                            AddToGrid();
+                            CheckForLines();
+                            this.enabled = false;
+                            FindObjectOfType<Spawner>().NewQube();
+                        }
+                        previousUnitPosition = t.position;
+                        moved = true;
                     }
                 }
-                   moved = false;
-                        
+
+
+
+                else if (t.phase == TouchPhase.Ended)
+                {
+                    if (!moved && t.position.x > Screen.width / 4)
+                    {
+                        transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
+                        if (!ValidMove())
+                        {
+                            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
+                        }
+                    }
+                    moved = false;
+
+                }
+
             }
-             
-        }
-        
+        } 
     }
 
     void CheckForLines()
